5ada93c722cf052260fbb567f44dcee74e25aa26
Fabho <fabian.calsina@gmail.com>
2017-11-29 18:04:20 -0400

    displaying blog post

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=fabhotest';
@@ -22,3 +23,4 @@ export function createPost(props) {
 		payload: request
 	};
 }
+
